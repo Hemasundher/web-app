5776294b356e3adb53a147b079dacea584e6fd7d
Hemasundher <89529752+Hemasundher@users.noreply.github.com>
2024-05-16 14:20:13 +0000

    first

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return 'Hello from Flask!'
+def hello():
+    return 'Hello!'
+
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
